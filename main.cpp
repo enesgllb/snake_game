2ffd722ba0d6904bca59975383bbe279a9d96f00
enes <enesgulleb@gmail.com>
2024-12-03 18:09:52 +0300

    2_add_background_second_edit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
+#include "Background.h"
 
 int main() {
+	const int gridRowSize{50}; // x
+	const int gridColSize{15}; // y
+	const char fillingCharacter{'x'};
 
+	Background background(gridRowSize, gridColSize, fillingCharacter);
+	background.print_grid();
 	return 0;
 }
